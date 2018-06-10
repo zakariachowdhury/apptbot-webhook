c9be7a83db61ec95dc553ff9fa783543d5de476b
Zakaria Chowdhury <zakaria.cse@gmail.com>
2018-06-10 18:35:01 -0400

    Add page access token and get sender PSID

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
 
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
@@ -20,10 +22,14 @@ app.post('/webhook', (req, res) => {
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
 
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
+      // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
+
     });
 
     // Returns a '200 OK' response to all requests
@@ -39,7 +45,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = process.env.VERIFY_TOKEN
+  const VERIFY_TOKEN = process.env.VERIFY_TOKEN
     
   // Parse the query params
   let mode = req.query['hub.mode'];
@@ -63,4 +69,17 @@ app.get('/webhook', (req, res) => {
   }
 });
 
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
 
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}
